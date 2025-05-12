b52b111aab9473c8abdd4286963577d4a77a3d37
Kristina Tomicic <54735031+krista-t@users.noreply.github.com>
2025-05-12 11:46:33 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -121,7 +121,7 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      Plugin.Favicon(),
+      Plugin.Favicon({icon: "static/icon.png"}),
       Plugin.NotFoundPage(),
       Plugin.CustomOgImages(),
     ],
